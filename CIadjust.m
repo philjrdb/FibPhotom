c6e975eb12d74601a7e5d11c7655cf74b2256ecd
philjrdb <57160185+philjrdb@users.noreply.github.com>
2021-11-18 10:29:44 +1100

    Update CIadjust.m

--- CIadjust.m
+++ CIadjust.m
@@ -16,9 +16,10 @@ function [adjLCI,adjUCI] = CIadjust(LCI,UCI,est,n,adj_type)
 % adjLCI = adjusted LCI
 % adjUCI = adjusted UCI
 
-%% GNU
-%  Copyright 2020 Philip Jean-Richard-dit-Bressel, UNSW Sydney
+%% By Philip Jean-Richard-dit-Bressel, UNSW Sydney, 2020
+% Feel free to use with citation: Jean-Richard-dit-Bressel et al. (2020). https://doi.org/10.3389/fnmol.2020.00014
 
+%% GNU
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
